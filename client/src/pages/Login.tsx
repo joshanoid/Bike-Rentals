28e14eb033168d6692ed4b74d3c283d7403a91fb
Jozsef Varga <joshanoid@gmail.com>
2022-09-02 23:11:56 +0200

    login page is nearly finished, handling request errors and storing auth data on success

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,22 +1,34 @@
 import * as React from 'react'
-import { Box, Button, Stack, TextField, Typography } from '@mui/material'
+import { Alert, Box, Button, Stack, TextField, Typography } from '@mui/material'
+import axios from 'axios'
+
+import { getErrorMessage } from 'shared/error'
+import { setAuthData } from 'utils/auth'
+import { User } from 'shared/types'
 
 export const Login = () => {
     const usernameRef = React.useRef<HTMLInputElement>(null)
     const passwordRef = React.useRef<HTMLInputElement>(null)
+    const [requestError, setRequestError] = React.useState('')
 
-    const onLogin = () => {
+    const onClick = async (mode: 'login' | 'register') => {
         const username = usernameRef.current?.value
         const password = passwordRef.current?.value
 
-        console.log(username, password)
-    }
+        setRequestError('')
 
-    const onRegister = () => {
-        const username = usernameRef.current?.value
-        const password = passwordRef.current?.value
+        try {
+            const {
+                data: { user, token },
+            } = await axios.post<{ user: User; token: string }>(`${process.env.API_URL}/auth/${mode}`, {
+                username,
+                password,
+            })
 
-        console.log(username, password)
+            setAuthData(user, token)
+        } catch (error) {
+            setRequestError(getErrorMessage(error))
+        }
     }
 
     return (
@@ -24,8 +36,16 @@ export const Login = () => {
             <Typography variant="h2">Login</Typography>
             <Box>
                 <Stack spacing={2}>
-                    <TextField required fullWidth label="Username" name="username" inputRef={usernameRef} />
                     <TextField
+                        error={Boolean(requestError)}
+                        required
+                        fullWidth
+                        label="Username"
+                        name="username"
+                        inputRef={usernameRef}
+                    />
+                    <TextField
+                        error={Boolean(requestError)}
                         required
                         fullWidth
                         name="password"
@@ -33,10 +53,11 @@ export const Login = () => {
                         type="password"
                         inputRef={passwordRef}
                     />
-                    <Button fullWidth variant="contained" onClick={onLogin}>
+                    {requestError && <Alert severity="error">{requestError}</Alert>}
+                    <Button fullWidth variant="contained" onClick={() => onClick('login')}>
                         Login
                     </Button>
-                    <Button fullWidth variant="contained" onClick={onRegister}>
+                    <Button fullWidth variant="contained" onClick={() => onClick('register')}>
                         Register
                     </Button>
                 </Stack>
