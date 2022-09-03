4ecf9f9cd109b2a2ed87513daa454835bd3feafd
Jozsef Varga <joshanoid@gmail.com>
2022-09-03 16:44:24 +0200

    added the ability to use protected routes and set auth details on login, clear it on logout

--- client/src/components/Router.tsx
+++ client/src/components/Router.tsx
@@ -1,12 +1,41 @@
 import * as React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Navigate, Routes, Route } from 'react-router-dom'
 
 import { Home } from 'pages/Home'
 import { Login } from 'pages/Login'
+import { Logout } from 'pages/Logout'
+import { useAuthContext } from 'utils/auth'
+import { Auth } from 'utils/types'
 
-export const Router = () => (
+type ProtectedRouteProps = {
+    children: JSX.Element
+}
+
+const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
+    const auth = useAuthContext()
+
+    return auth ? children : <Navigate to="/login" />
+}
+
+const Dashboard = () => <div>Im super protected</div>
+
+type Props = {
+    setAuth: (authObject: Auth) => void
+}
+
+export const Router = ({ setAuth }: Props) => (
     <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login setAuth={setAuth} />} />
+        <Route path="/logout" element={<Logout setAuth={setAuth} />} />
+        <Route
+            path="/dashboard"
+            element={
+                <ProtectedRoute>
+                    <Dashboard />
+                </ProtectedRoute>
+            }
+        />
+        <Route path="*" element={<Navigate to="/login" />} />
     </Routes>
 )
