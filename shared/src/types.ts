42e8e73c295f4d264e8833388f0c0a8f8fd18ecc
Jozsef Varga <joshanoid@gmail.com>
2022-09-03 20:13:25 +0200

    added bike and rating types

--- shared/src/types.ts
+++ shared/src/types.ts
@@ -3,3 +3,15 @@ export type User = {
     password: string
     type: 'manager' | 'user'
 }
+
+export type Rating = {
+    username: string
+    value: 1 | 2 | 3 | 4 | 5
+}
+
+export type Bike = {
+    model: string
+    color: string
+    location: string
+    ratings: ReadonlyArray<Rating>
+}
