3d8da9c2f778abba69ea2bcffd2751ab366a9be3
Jozsef Varga <joshanoid@gmail.com>
2022-09-03 16:47:01 +0200

    layout upgraded with using auth data to be able to render conditionally links

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,24 +1,31 @@
+import * as React from 'react'
 import { Box, Stack, Typography } from '@mui/material'
 import { Link } from 'react-router-dom'
-import * as React from 'react'
+
+import { useAuthContext } from 'utils/auth'
 
 type Props = {
     children: React.ReactNode
 }
 
-export const Layout = ({ children }: Props) => (
-    <Box>
-        <Stack direction="column">
-            <Box>
-                <Typography variant="h1" align="center">
-                    React Bike Rental
-                </Typography>
-            </Box>
-            <Stack padding={2} direction="row" spacing={1} justifyContent="center">
-                <Link to="/">Home</Link>
-                <Link to="/login">Login</Link>
+export const Layout = ({ children }: Props) => {
+    const auth = useAuthContext()
+
+    return (
+        <Box>
+            <Stack direction="column">
+                <Box>
+                    <Typography variant="h1" align="center">
+                        React Bike Rental
+                    </Typography>
+                </Box>
+                <Stack padding={2} direction="row" spacing={1} justifyContent="center">
+                    <Link to="/">Home</Link>
+                    {auth ? <Link to="/logout">Logout</Link> : <Link to="/login">Login</Link>}
+                    {auth && <Link to="/dashboard">Dashboard</Link>}
+                </Stack>
+                <Box padding={2}>{children}</Box>
             </Stack>
-            <Box padding={2}>{children}</Box>
-        </Stack>
-    </Box>
-)
+        </Box>
+    )
+}
