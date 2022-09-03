88d27584e5b66668d1a01fa3a72b96e75d6c59a2
Jozsef Varga <joshanoid@gmail.com>
2022-09-03 16:46:26 +0200

    auth utility added which provides an auth context and a hook to handle auth data in localstorage

---
+++ client/src/utils/auth.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react'
+
+import { Auth } from './types'
+
+const AuthContext = React.createContext<Auth>(undefined)
+
+type Props = {
+    children: React.ReactNode
+    auth: Auth
+}
+
+export const AuthProvider = ({ children, auth }: Props) => (
+    <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>
+)
+
+export const useAuthContext = () => React.useContext(AuthContext)
+
+export const useAuth = (): [Auth, (authObject: Auth) => void] => {
+    const getAuth = (): Auth => {
+        const auth = localStorage.getItem('auth')
+
+        if (auth) {
+            return JSON.parse(auth)
+        }
+
+        return undefined
+    }
+
+    const [auth, setAuth] = React.useState<Auth>(getAuth())
+
+    const saveAuth = (authObject: Auth) => {
+        if (authObject) {
+            localStorage.setItem('auth', JSON.stringify(authObject))
+        } else {
+            localStorage.removeItem('auth')
+        }
+
+        setAuth(authObject)
+    }
+
+    return [auth, saveAuth]
+}
