1f57fd96efd85fdeb1562b6a8b8a8dd5d4803a25
Jozsef Varga <joshanoid@gmail.com>
2022-09-05 01:23:16 +0200

    added post, put and delete routes for bike api

--- server/src/routes/bikes.ts
+++ server/src/routes/bikes.ts
@@ -1,14 +1,45 @@
 import * as express from 'express'
 
-import { authenticateJWTToken } from 'utils/auth'
+import { authenticateJWTToken, authenticateManagerJWTToken } from 'utils/auth'
 import BikeModel from 'models/bike'
+import { getErrorMessage } from 'shared/error'
+import { Bike } from 'shared/types'
 
 const router: express.Router = express.Router()
 
-router.get('/bikes', authenticateJWTToken, async (_req: express.Request, res: express.Response) => {
-    const bikes = await BikeModel.find()
+router
+    .get('/bikes', authenticateJWTToken, async (_req: express.Request, res: express.Response) => {
+        const bikes = await BikeModel.find()
 
-    res.status(200).json(bikes)
-})
+        res.status(200).json(bikes)
+    })
+    .post('/bikes', authenticateManagerJWTToken, async (req: express.Request, res: express.Response) => {
+        try {
+            const { model, color, location } = req.body as Bike
+            const newBike = new BikeModel({ model, color, location })
+
+            const insertedBike = await newBike.save()
+
+            res.status(201).json(insertedBike)
+        } catch (error) {
+            res.status(400).json({ message: getErrorMessage(error) })
+        }
+    })
+    .put('/bikes/:id', authenticateManagerJWTToken, async (req: express.Request, res: express.Response) => {
+        const { id } = req.params
+        const { model, color, location } = req.body
+
+        await BikeModel.findByIdAndUpdate(id, { model, color, location })
+
+        const updatedBike = await BikeModel.findById(id)
+
+        res.status(200).json(updatedBike)
+    })
+    .delete('/bikes/:id', authenticateManagerJWTToken, async (req: express.Request, res: express.Response) => {
+        const { id } = req.params
+        const deletedBike = await BikeModel.findByIdAndDelete(id)
+
+        res.status(200).json(deletedBike)
+    })
 
 export default router
