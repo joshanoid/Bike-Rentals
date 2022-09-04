7839c4e07f569f8a0bc084d0f2a7ce3b75413a2c
Jozsef Varga <joshanoid@gmail.com>
2022-09-04 14:11:12 +0200

    date range type added

--- client/src/pages/Dashboard/types.ts
+++ client/src/pages/Dashboard/types.ts
@@ -1,3 +1,5 @@
 import { Bike } from 'shared/types'
 
 export type ExtendedBike = Bike & { _id: string; averageRating: number | null; canRate: boolean }
+
+export type DateRange = [Date | null, Date | null]
